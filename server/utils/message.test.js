a968bfbf04aa2aca13999ce872c369c5a349ee16
kwenealete <kwenait@yahoo.ca>
2019-02-12 12:06:39 +0100

    created generateMessage utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,13 @@
+let expect = require('expect');
+
+let {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+    it('should generate correct message object', () => {
+        let from = 'monya';
+        let text = 'Some message';
+        let message = generateMessage(from, text);
+       expect(message.createdAt).toBeA('number');
+       expect(message).toInclude({from, text});
+    });
+});
